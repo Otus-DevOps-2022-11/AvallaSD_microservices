82b478cf211cec517038ca80622b8419d99ff2b1
AvallaSD <avallakittishvarra@gmail.com>
2023-03-27 11:42:29 +0000

    src updated

--- src/comment/docker_build.sh
+++ src/comment/docker_build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-export USER_NAME=avallasd
 
 echo `git show --format="%h" HEAD | head -1` > build_info.txt
 echo `git rev-parse --abbrev-ref HEAD` >> build_info.txt
 
-docker build -t $USER_NAME/comment .
+docker build -t $USER_NAME/comment:logging .
